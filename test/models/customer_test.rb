44e773dbacfe62049b2ac700c543935686d66380
Ting Wong <hellotingwong@gmail.com>
2017-05-09 16:52:17 -0700

    added first round of model tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,51 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
+  describe "validations" do
+    it "Customer is invalid without a name" do
+      customer = Customer.new
+      result = customer.valid?
+      result.must_equal false
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+      customer.errors.messages.must_include :name
+    end
+
+    it "Creates Customer with name" do
+      customer = Customer.create(name: "Sofia")
+      customer.errors.messages.wont_include :name
+    end
+
+    it "Customer is invalid without an empty name field" do
+      customer = Customer.new(name: "")
+      result = customer.valid?
+      result.must_equal false
+
+      customer.errors.messages.must_include :name
+    end
+
+    it "Customer is invalid without a phone" do
+      customer = Customer.new
+      result = customer.valid?
+      result.must_equal false
+
+      customer.errors.messages.must_include :phone
+    end
+
+    it "Creates Customer with phone" do
+      customer = Customer.create(phone: "Sofia")
+      customer.errors.messages.wont_include :phone
+    end
+
+    it "Customer is invalid without an empty phone field" do
+      customer = Customer.new(phone: "")
+      result = customer.valid?
+      result.must_equal false
+
+      customer.errors.messages.must_include :phone
+    end
+  end
+
+  describe "relationships" do
+    
   end
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,47 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  describe "validations" do
+    it "Movie is invalid without a title" do
+      movie = Movie.new
+      result = movie.valid?
+      result.must_equal false
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+      movie.errors.messages.must_include :title
+    end
+
+    it "Creates Movie with title" do
+      movie = Movie.create(title: "SofiaMovie")
+      movie.errors.messages.wont_include :title
+    end
+
+    it "Movie is invalid without an empty title field" do
+      movie = Movie.new(title: "")
+      result = movie.valid?
+      result.must_equal false
+
+      movie.errors.messages.must_include :title
+    end
+
+    it "Movie is invalid without a inventory" do
+      movie = Movie.new
+      result = movie.valid?
+      result.must_equal false
+
+      movie.errors.messages.must_include :inventory
+    end
+
+    it "Creates Movie with inventory" do
+      movie = Movie.create(inventory: "Sofia")
+      movie.errors.messages.wont_include :inventory
+    end
+
+    it "Movie is invalid without an empty inventory field" do
+      movie = Movie.new(inventory: "")
+      result = movie.valid?
+      result.must_equal false
+
+      movie.errors.messages.must_include :inventory
+    end
   end
 end
