a3ded28844578d84a19211f77f359a93f1e1c590
Sofia Kim <sofiamhkim@gmail.com>
2017-05-10 14:58:13 -0700

    controller test completed

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "is successful renders when there are many customers" do
+      Customer.count.must_be :>, 0
+      get customers_path
+      must_respond_with :success
+    end
+
+    it "is successful when there are zero customer" do
+      Customer.destroy_all
+      get customers_path
+      must_respond_with :success
+    end
+  end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "is successful renders when there are many movies" do
+      Movie.count.must_be :>, 0
+      get movies_path
+      must_respond_with :success
+    end
+
+    it "is successful when there are zero movie" do
+      Movie.destroy_all
+      get movies_path
+      must_respond_with :success
+    end
+  end
 end
