7254b8b318ec2be737c36a1f64a103464f87ed71
Ting Wong <hellotingwong@gmail.com>
2017-05-10 15:45:40 -0700

    added controller tests

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -8,10 +8,36 @@ describe CustomersController do
       must_respond_with :success
     end
 
-    it "is successful when there are zero customer" do
+    it "is successful when there are zero customers" do
       Customer.destroy_all
       get customers_path
       must_respond_with :success
+    end
+
+    it "returns JSON" do
+      get customers_url
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an array of customers" do
+      get customers_url
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the customers" do
+      get customers_url
+      body = JSON.parse(response.body)
+      body.length.must_equal Customer.count
+    end
+
+    it "returns customers with the right fields" do
+      fields = %w(id movies_checked_out_count name phone postal_code registered_at)
+      get customers_url
+      body = JSON.parse(response.body)
+      body.each do |customer|
+        customer.keys.sort.must_equal fields
+      end
     end
   end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -13,5 +13,31 @@ describe MoviesController do
       get movies_path
       must_respond_with :success
     end
+
+    it "returns JSON" do
+      get movies_url
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an array of movies" do
+      get movies_url
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the movies" do
+      get movies_url
+      body = JSON.parse(response.body)
+      body.length.must_equal Movie.count
+    end
+
+    it "returns movies with the right fields" do
+      fields = %w(available_inventory inventory overview release_date title)
+      get movies_url
+      body = JSON.parse(response.body)
+      body.each do |movie|
+        movie.keys.sort.must_equal fields
+      end
+    end
   end
 end
