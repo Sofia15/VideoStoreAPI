3159911e712b919c8e05d41205a8eefdc49de168
Ting Wong <hellotingwong@gmail.com>
2017-05-11 09:07:50 -0700

    modified customer serializer

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,4 +1,3 @@
 class CustomerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count
-  # has_many :movies
+  attributes :id, :name, :registered_at, :postal_code, :phone
 end
