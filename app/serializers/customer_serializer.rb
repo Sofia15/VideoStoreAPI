d25ef7f3452508eab8c86bfd913329cfbee973eb
Ting Wong <hellotingwong@gmail.com>
2017-05-11 09:14:37 -0700

    all tests passing

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,3 +1,3 @@
 class CustomerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :registered_at, :postal_code, :phone
+  attributes :id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count
 end
