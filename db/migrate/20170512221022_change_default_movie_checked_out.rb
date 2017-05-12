0e6a782cf313426c6db6a46c1d3ac46b0ff21060
Ting Wong <hellotingwong@gmail.com>
2017-05-12 15:12:03 -0700

    optional-1

---
+++ db/migrate/20170512221022_change_default_movie_checked_out.rb
@@ -0,0 +1,5 @@
+class ChangeDefaultMovieCheckedOut < ActiveRecord::Migration[5.0]
+  def change
+    change_column_default :customers, :movies_checked_out_count, 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512190037) do
+ActiveRecord::Schema.define(version: 20170512221022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,9 +23,9 @@ ActiveRecord::Schema.define(version: 20170512190037) do
     t.string   "state"
     t.string   "postal_code"
     t.string   "phone"
-    t.integer  "movies_checked_out_count"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.integer  "movies_checked_out_count", default: 0
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.float    "account_credit"
   end
 
